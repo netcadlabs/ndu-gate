a545b2d04ad59a7feaafc2bd7e94a50f86360db5
agtokty <agitoktay@gmail.com>
2020-10-06 02:07:40 +0300

    version 0.0.9

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-VERSION = "0.0.8"
+VERSION = "0.0.9"
 
 setup(
     name='ndu-gate',
-    version='0.0.8',
+    version='0.0.9',
     license='Apache Software License (Apache Software License 2.0)',
     author='Netcad Innovation Labs @NETCAD',
     author_email='netcadinnovationlabs@gmail.com',
