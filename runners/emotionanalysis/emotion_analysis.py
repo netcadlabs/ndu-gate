9b6866f9880bd8c0a1276ae56297a10cda31713d
agit oktay <agitoktay@gmail.com>
2020-10-21 13:33:18 +0300

    emotion analysis refactor

--- runners/emotionanalysis/emotion_analysis.py
+++ runners/emotionanalysis/emotion_analysis.py
@@ -1,6 +1,9 @@
 from threading import Thread
 from random import choice
 from string import ascii_lowercase
+import os
+import zipfile
+from pathlib import Path
 
 from ndu_gate_camera.utility.ndu_utility import NDUUtility
 
@@ -25,8 +28,7 @@ class EmotionAnalysisRunner(Thread, NDUCameraRunner):
         # self.__personDetector = PersonDetector()
         # self.__faceDetector = FaceDetector()
 
-        from .emotional_model import load_emotion_model
-        self.__emotion_model = load_emotion_model()
+        self.__emotion_model = self.load_emotion_model()
         self.__emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
 
     def get_name(self):
@@ -82,3 +84,72 @@ class EmotionAnalysisRunner(Thread, NDUCameraRunner):
             emotions_sum[6] += 1
             color = (255, 0, 255)
             return "Error", color
+
+
+
+    try:
+        import tensorflow as tf
+    except ImportError:
+        if NDUUtility.install_package("tensorflow") == 0:
+            import tensorflow as tf
+
+
+    def load_emotion_model(self):
+        num_classes = 7
+
+        model = tf.keras.models.Sequential()
+
+        # 1st convolution layer
+        model.add(tf.keras.layers.Conv2D(64, (5, 5), activation='relu', input_shape=(48, 48, 1)))
+        model.add(tf.keras.layers.MaxPooling2D(pool_size=(5, 5), strides=(2, 2)))
+
+        # 2nd convolution layer
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+        # 3rd convolution layer
+        model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+        model.add(tf.keras.layers.AveragePooling2D(pool_size=(3, 3), strides=(2, 2)))
+
+        model.add(tf.keras.layers.Flatten())
+
+        # fully connected neural networks
+        model.add(tf.keras.layers.Dense(1024, activation='relu'))
+        model.add(tf.keras.layers.Dropout(0.2))
+        model.add(tf.keras.layers.Dense(1024, activation='relu'))
+        model.add(tf.keras.layers.Dropout(0.2))
+
+        model.add(tf.keras.layers.Dense(num_classes, activation='softmax'))
+
+        local_facial_weights_file = os.path.dirname(os.path.abspath(__file__)) + "/data/deepface/facial_expression_model_weights.h5".replace("/", os.path.sep)
+
+        facial_weights_file = os.path.abspath(os.path.abspath(__file__) + "/data/deepface/facial_expression_model_weights.h5".replace("/", os.path.sep))
+
+        facial_weights_file_final = None
+
+        if os.path.isfile(local_facial_weights_file) is True:
+            facial_weights_file_final = local_facial_weights_file
+        elif os.path.isfile(facial_weights_file) is False:
+            home = str(Path.home())
+            facial_weights_file = home + ('/.deepface/weights/facial_expression_model_weights.h5').replace("/", os.path.sep)
+            if os.path.isfile(facial_weights_file) is False:
+                print("facial_expression_model_weights.h5 will be downloaded...")
+
+                # zip
+                url = 'https://drive.google.com/uc?id=13iUHHP3SlNg53qSuQZDdHDSDNdBP9nwy'
+                output = facial_weights_file
+
+                from gdown import download as download_g
+                download_g(url, output, quiet=False)
+
+                # unzip facial_expression_model_weights.zip
+                with zipfile.ZipFile(output, 'r') as zip_ref:
+                    zip_ref.extractall(home + '/.deepface/weights/')
+
+            facial_weights_file_final = facial_weights_file
+
+        model.load_weights(facial_weights_file_final)
+
+        return model
