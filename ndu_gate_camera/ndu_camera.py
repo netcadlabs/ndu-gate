ea5c30ae238795d5de46c44d85087eabc9e3b8a9
agtokty <agitoktay@gmail.com>
2020-12-06 22:15:53 +0300

    add traceback if exit

--- ndu_gate_camera/ndu_camera.py
+++ ndu_gate_camera/ndu_camera.py
@@ -1,5 +1,6 @@
 import getopt
 import sys
+import traceback
 from os import path, listdir, mkdir, curdir
 from ndu_gate_camera.camera.ndu_camera_service import NDUCameraService
 from ndu_gate_camera.utility.constants import DEFAULT_NDU_GATE_CONF
@@ -26,7 +27,13 @@ def main(argv):
             config_file_name = "ndu_gate_debug.yaml"
         ndu_gate_config_file = path.dirname(path.abspath(__file__)) + '/config/'.replace('/', path.sep) + config_file_name
 
-    NDUCameraService(ndu_gate_config_file=ndu_gate_config_file)
+    try:
+        NDUCameraService(ndu_gate_config_file=ndu_gate_config_file)
+    except Exception as e:
+        print("NDUCameraService PATLADI")
+        print(e)
+        print("----------------------")
+        print(traceback.format_exc())
 
 
 def daemon():
